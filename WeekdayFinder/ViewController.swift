74c7f3b2c5a0fdddf4bbe25599ecc1754fd12916
Nikita Gruzdev <nikitulka83@gmail.com>
2020-07-26 12:49:49 +0300

    First commit

---
+++ WeekdayFinder/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Никита Груздев on 26.07.2020.
+//  Copyright © 2020 Никита Груздев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    
+}
+
